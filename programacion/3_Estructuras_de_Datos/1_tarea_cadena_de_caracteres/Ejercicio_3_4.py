cb63b5bdd30b47c17bfd1fc78e07ffd0df9c828e
jav <javierlopeztl1@gmail.com>
2026-01-13 21:00:05 +0100

    Clase 13-01-26

---
+++ programacion/3_Estructuras_de_Datos/1_tarea_cadena_de_caracteres/Ejercicio_3_4.py
@@ -0,0 +1,5 @@
+# Hay un método de cadenas llamado find, que es similar a count.
+# Escribe el código necesario para invocar a este método find y contar el número de 
+# veces que una letra aparece en “banana”.
+
+palabra = banana
---
+++ programacion/3_Estructuras_de_Datos/2_Ejercicios_Listas_Tuplas/Ejercicio_3_1_2.py
@@ -0,0 +1,9 @@
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo
+# Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla
+# el mensaje Yo estudio <asignatura>, donde <asignatura> sobre cada una de las
+# asignaturas de la lista.
+
+asignaturas = ["Matematicas","Fisica","Quimica","Historia","Lengua"]
+
+for i in range (0,len(asignaturas)):
+    print(f"Yo estudio {asignaturas[i]}")
