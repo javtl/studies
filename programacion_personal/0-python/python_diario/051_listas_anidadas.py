d9e07255d321b49938258de22785b19a6718f457
jav <javierlopeztl1@gmail.com>
2026-02-03 18:37:55 +0100

    ej 51

---
+++ programacion_personal/0-python/python_diario/051_listas_anidadas.py
@@ -0,0 +1,20 @@
+# Crea una pequeña matriz de 3x3 y accede a sus valores:
+# Crea una lista llamada matriz que contenga tres listas dentro, cada una con tres números (ej: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]).
+# Imprime la segunda fila completa de la matriz.
+# Accede e imprime solo el número 6 (que está en la segunda fila, tercera columna).
+# Accede e imprime solo el número 7 (que está en la tercera fila, primera columna).
+# Extra: Usa un bucle for para imprimir cada "fila" de la matriz en una línea distinta.
+
+numeros = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+
+# impimir segunda fila
+print(numeros[1])
+# imprimir numero 6
+print(numeros[1][2])
+# imprimir numero 7
+print(numeros[2][0])
+
+
