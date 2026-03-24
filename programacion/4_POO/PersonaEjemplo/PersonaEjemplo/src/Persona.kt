a753c46df5ceaedfea33670f88d70f6a1b99c482
jav <javierlopeztl1@gmail.com>
2026-03-24 21:04:25 +0100

    contructors

--- programacion/4_POO/PersonaEjemplo/PersonaEjemplo/src/Persona.kt
+++ programacion/4_POO/PersonaEjemplo/PersonaEjemplo/src/Persona.kt
@@ -1,11 +1,15 @@
-/**
- *
- * */
-
 
 class Persona (val nombre: String , var edad: Int){
 
+    var email: String? = null
+    constructor(nombre: String, edad: Int, email: String) : this(nombre, edad) {
+        this.email = email
+    }
+
     public fun presentarse(){
         print("Hola me llamo $nombre y tengo $edad años")
+        if (this.email != null ){
+            println("Mi email es: ${this.email}")
+        }
     }
 }
